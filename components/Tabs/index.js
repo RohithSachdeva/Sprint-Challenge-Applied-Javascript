fea7361a0b602d95a9e287f2559ce54d45e8b61e
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-13 08:57:50 -0700

    before starting tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//array1.forEach(element => console.log(element)); :::: syntax
+
+// axios.get('url')    .then(response =>{})
+//console.log(response.data);  so data.topics? 
+// //Object
+// topics: Array(5)
+// 0: "javascript"
+// 1: "bootstrap"
+// 2: "technology"
+// 3: "jquery"
+// 4: "node.js"
+// so response.data.topics to a const ... loop thru that?  or can create component that refers to the `${element} and appends it .. still need foreach loop
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+
+.then(response => {
+    response.data.topics  
+})
+
